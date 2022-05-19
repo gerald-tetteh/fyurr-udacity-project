d46de084c6629ab1ba7de301b0be2b2963ebb45b
Gerald Addo-Tetteh <geraldadt@outlook.com>
2022-05-19 00:18:29 +0000

    complete create shoe controller

--- app.py
+++ app.py
@@ -561,15 +561,30 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-    # called to create new shows in the db, upon submitting new show listing form
-    # TODO: insert form data as a new Show record in the db, instead
-
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-    return render_template('pages/home.html')
+    error = False
+    form = ShowForm()
+    if form.validate_on_submit():
+        try:
+            show = Show(
+                start_time=form.start_time.data,
+                artist_id=form.artist_id.data,
+                venue_id=form.venue_id.data
+            )
+            db.session.add(show)
+            db.session.commit()
+            flash('Show was successfully listed!')
+        except:
+            error = True
+            db.session.rollback()
+            flash('An error occurred. Show could not be listed.')
+        finally:
+            db.session.close()
+        if error:
+            abort(500)
+    else:
+        flash("Some fields failed validation")
+        return render_template('forms/new_show.html', form=form)
+    return redirect(url_for("index"))
 
 
 @app.errorhandler(404)
