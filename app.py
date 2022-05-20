db66ae72d2ca81782e7914626773f573a36709dd
Gerald Addo-Tetteh <geraldadt@outlook.com>
2022-05-20 19:25:06 +0000

    complete search feature

--- app.py
+++ app.py
@@ -70,16 +70,22 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for Hop should return "The Musical Hop".
-    # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+    search_query = Venue.query.with_entities(
+        Venue.id, Venue.name,
+        db.func.count(db.case((Show.start_time > datetime.now(), 1)))
+        .label("num_upcoming_shows"))
+    search_term = request.form.get("search_term", "")
+    if "," in search_term:
+        city, state = [string.strip() for string in search_term.split(",")]
+        search_query = search_query.filter(
+            Venue.city.ilike(city), Venue.state.ilike(state))
+    else:
+        search_query = search_query.filter(
+            Venue.name.ilike(f"%{search_term}%"))
+    venues = search_query.outerjoin(Show).group_by(Venue.id).all()
     response = {
-        "count": 1,
-        "data": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(venues),
+        "data": [venue._asdict() for venue in venues]
     }
     return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
@@ -200,16 +206,22 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+    search_query = Artist.query.with_entities(
+        Artist.id, Artist.name,
+        db.func.count(db.case((Show.start_time > datetime.now(), 1)))
+        .label("num_upcoming_shows"))
+    search_term = request.form.get("search_term", "")
+    if "," in search_term:
+        city, state = [string.strip() for string in search_term.split(",")]
+        search_query = search_query.filter(
+            Artist.city.ilike(city), Artist.state.ilike(state))
+    else:
+        search_query = search_query.filter(
+            Artist.name.ilike(f"%{search_term}%"))
+    artists = search_query.outerjoin(Show).group_by(Artist.id).all()
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(artists),
+        "data": [artist._asdict() for artist in artists]
     }
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
